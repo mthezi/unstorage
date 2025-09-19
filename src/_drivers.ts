7045fd8740694146b251c64475157d100fc5cba5
mthezi <mthezi@163.com>
2025-09-19 17:38:35 +0800

    fix: lint and publish

--- src/_drivers.ts
+++ src/_drivers.ts
@@ -4,7 +4,7 @@
 import type { FSStorageOptions as FsLiteOptions } from "unstorage/drivers/fs-lite";
 import type { FSStorageOptions as FsOptions } from "unstorage/drivers/fs";
 
-export type BuiltinDriverName = "azure-app-configuration" | "azureAppConfiguration" | "azure-cosmos" | "azureCosmos" | "azure-key-vault" | "azureKeyVault" | "azure-storage-blob" | "azureStorageBlob" | "azure-storage-table" | "azureStorageTable" | "capacitor-preferences" | "capacitorPreferences" | "cloudflare-kv-binding" | "cloudflareKVBinding" | "cloudflare-kv-http" | "cloudflareKVHttp" | "cloudflare-r2-binding" | "cloudflareR2Binding" | "db0" | "deno-kv-node" | "denoKVNode" | "deno-kv" | "denoKV" | "fs-lite" | "fsLite" | "fs" | "github" | "http" | "indexedb" | "localstorage" | "lru-cache" | "lruCache" | "memory" | "mongodb" | "netlify-blobs" | "netlifyBlobs" | "null" | "overlay" | "planetscale" | "redis" | "s3" | "session-storage" | "sessionStorage" | "uploadthing" | "upstash" | "vercel-blob" | "vercelBlob" | "vercel-kv" | "vercelKV" | "vercel-runtime-cache" | "vercelRuntimeCache";
+export type BuiltinDriverName = "fs-lite" | "fsLite" | "fs" | "memory";
 
 export type BuiltinDriverOptions = {
   "fs-lite": FsLiteOptions;
@@ -13,55 +13,8 @@ export type BuiltinDriverOptions = {
 };
 
 export const builtinDrivers = {
-  "azure-app-configuration": "unstorage/drivers/azure-app-configuration",
-  "azureAppConfiguration": "unstorage/drivers/azure-app-configuration",
-  "azure-cosmos": "unstorage/drivers/azure-cosmos",
-  "azureCosmos": "unstorage/drivers/azure-cosmos",
-  "azure-key-vault": "unstorage/drivers/azure-key-vault",
-  "azureKeyVault": "unstorage/drivers/azure-key-vault",
-  "azure-storage-blob": "unstorage/drivers/azure-storage-blob",
-  "azureStorageBlob": "unstorage/drivers/azure-storage-blob",
-  "azure-storage-table": "unstorage/drivers/azure-storage-table",
-  "azureStorageTable": "unstorage/drivers/azure-storage-table",
-  "capacitor-preferences": "unstorage/drivers/capacitor-preferences",
-  "capacitorPreferences": "unstorage/drivers/capacitor-preferences",
-  "cloudflare-kv-binding": "unstorage/drivers/cloudflare-kv-binding",
-  "cloudflareKVBinding": "unstorage/drivers/cloudflare-kv-binding",
-  "cloudflare-kv-http": "unstorage/drivers/cloudflare-kv-http",
-  "cloudflareKVHttp": "unstorage/drivers/cloudflare-kv-http",
-  "cloudflare-r2-binding": "unstorage/drivers/cloudflare-r2-binding",
-  "cloudflareR2Binding": "unstorage/drivers/cloudflare-r2-binding",
-  "db0": "unstorage/drivers/db0",
-  "deno-kv-node": "unstorage/drivers/deno-kv-node",
-  "denoKVNode": "unstorage/drivers/deno-kv-node",
-  "deno-kv": "unstorage/drivers/deno-kv",
-  "denoKV": "unstorage/drivers/deno-kv",
   "fs-lite": "unstorage/drivers/fs-lite",
   "fsLite": "unstorage/drivers/fs-lite",
   "fs": "unstorage/drivers/fs",
-  "github": "unstorage/drivers/github",
-  "http": "unstorage/drivers/http",
-  "indexedb": "unstorage/drivers/indexedb",
-  "localstorage": "unstorage/drivers/localstorage",
-  "lru-cache": "unstorage/drivers/lru-cache",
-  "lruCache": "unstorage/drivers/lru-cache",
   "memory": "unstorage/drivers/memory",
-  "mongodb": "unstorage/drivers/mongodb",
-  "netlify-blobs": "unstorage/drivers/netlify-blobs",
-  "netlifyBlobs": "unstorage/drivers/netlify-blobs",
-  "null": "unstorage/drivers/null",
-  "overlay": "unstorage/drivers/overlay",
-  "planetscale": "unstorage/drivers/planetscale",
-  "redis": "unstorage/drivers/redis",
-  "s3": "unstorage/drivers/s3",
-  "session-storage": "unstorage/drivers/session-storage",
-  "sessionStorage": "unstorage/drivers/session-storage",
-  "uploadthing": "unstorage/drivers/uploadthing",
-  "upstash": "unstorage/drivers/upstash",
-  "vercel-blob": "unstorage/drivers/vercel-blob",
-  "vercelBlob": "unstorage/drivers/vercel-blob",
-  "vercel-kv": "unstorage/drivers/vercel-kv",
-  "vercelKV": "unstorage/drivers/vercel-kv",
-  "vercel-runtime-cache": "unstorage/drivers/vercel-runtime-cache",
-  "vercelRuntimeCache": "unstorage/drivers/vercel-runtime-cache",
 } as const;
